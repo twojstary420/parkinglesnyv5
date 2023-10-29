731923bb6e861a9c3991535b7ab0721fb62fe626
twojstary420 <lucyna.aleks@op.pl>
2023-10-29 23:41:50 +0100

    First commit

---
+++ Assets/BombManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BombManager : MonoBehaviour
+{
+    public int bombCount;
+    public TextMeshProUGUI bombText;
+    public GameObject autobus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bombText.text = "x " + bombCount.ToString();
+
+        if(bombCount == 5)
+        {
+            Collider2D autobusCollider = autobus.GetComponent<Collider2D>();
+            if (autobusCollider != null)
+            {
+                autobusCollider.enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private float Move;
+
+    public float jump;
+
+    public bool isJumping;
+
+    private Rigidbody2D rb;
+
+    public BombManager bm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            isJumping = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Bomb"))
+        {
+            Destroy(other.gameObject);
+            bm.bombCount++;
+        }
+    }
+}
